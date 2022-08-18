ba1b6d4cb318e02addd0de8afe17cf424fee733e
jhlsuper <jhlsuper@gmail.com>
2022-08-18 23:59:15 +0900

    init React portfolio

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Protfolio from "./components/portfolio/Portfolio";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Services() {
+  return <div>S</div>;
+}
+
+export default Services;
