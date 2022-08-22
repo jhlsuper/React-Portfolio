4b43dc65a74144e8f18d3467c45445885928db51
jhlsuper <jhlsuper@gmail.com>
2022-08-22 23:59:41 +0900

    nav setState and about

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,21 +1,53 @@
 import React from "react";
 import "./nav.css";
-import {BiHome} from 'react-icons/bi'
-import {AiOutlineUser} from 'react-icons/ai'
-import {BiBookAlt} from 'react-icons/bi'
-import {RiServiceFill} from 'react-icons/ri'
-import {TbMessageDots} from 'react-icons/tb'
+import { BiHome } from "react-icons/bi";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBookAlt } from "react-icons/bi";
+import { RiServiceFill } from "react-icons/ri";
+import { TbMessageDots } from "react-icons/tb";
+import { useState } from "react";
 
 function Nav() {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#"><BiHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><BiBookAlt/></a>
-      <a href="#services"><RiServiceFill/></a>
-      <a href="#contact"><TbMessageDots/></a>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <BiHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBookAlt />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <RiServiceFill />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <TbMessageDots />
+      </a>
     </nav>
-  )
+  );
 }
 
 export default Nav;
