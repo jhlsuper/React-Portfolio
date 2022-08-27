6c4d3ea0b0e8cb61c24f99cd6f3132a4982039f4
jhlsuper <jhlsuper@gmail.com>
2022-08-28 00:28:31 +0900

    portfoilo JSX CSS JS method

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,81 @@
 import React from "react";
+import IMG1 from "../../assets/anilog.png";
+import IMG2 from "../../assets/SyncFit_ICON.png";
+import "./portfolio.css";
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Anilog",
+    github: "https://github.com",
+    demo: "none",
+  },
+  {
+    id: 1,
+    image: IMG2,
+    title: "Anilog",
+    github: "https://github.com",
+    demo: "none",
+  },
+  {
+    id: 1,
+    image: IMG1,
+    title: "Anilog",
+    github: "https://github.com",
+    demo: "none",
+  },
+  {
+    id: 1,
+    image: IMG2,
+    title: "Anilog",
+    github: "https://github.com",
+    demo: "none",
+  },
+  {
+    id: 1,
+    image: IMG1,
+    title: "Anilog",
+    github: "https://github.com",
+    demo: "none",
+  },
+  {
+    id: 1,
+    image: IMG2,
+    title: "Anilog",
+    github: "https://github.com",
+    demo: "none",
+  },
+];
 
 function Portfolio() {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__items">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title}></img>
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default Portfolio;
